67a682ac169bdd5ebee75318424f66d5754c4b7c
misha213123 <misha0228050@gmail.com>
2026-07-19 14:40:27 +0200

    Load cinematic battle styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import './styles/global.css';
 import './styles/characters.css';
+import './styles/cinematic-battle.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
